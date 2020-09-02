831297c1c680054aa66d70bdb3398fdd858f905c
vktorbr <vktorbr@gmail.com>
2020-09-01 23:19:22 -0300

    adicao do botao mostrar/esconder detalhes

--- public/scripts.js
+++ public/scripts.js
@@ -2,7 +2,21 @@ const recipes = document.querySelectorAll('.recipe');
 for (const recipe of recipes) {
     recipe.addEventListener('click', function(){
         const cardID = recipe.getAttribute('id'); 
-       window.location.href = `/recipes/${cardID}`;
+        window.location.href = `/recipes/${cardID}`;
+    })
+}
+const abouts = document.querySelectorAll('.about');
+for (const about of abouts) {
+    const button = about.querySelector('.sub_button'); 
+    button.addEventListener('click', function(){
+        const description =  about.querySelector('.detail_description');
+        if(description.classList.contains('active')){
+            description.classList.remove('active')
+            button.querySelector('a').innerHTML = 'ESCONDER';
+        }else{
+            description.classList.add('active');
+            button.querySelector('a').innerHTML = 'MOSTRAR';
+        }
     })
 }
 
