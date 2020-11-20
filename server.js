d0e50f1dc2a6229dd7542857c3bcb193d5a403be
Victor Martins <vktor@arch.localdomain>
2020-11-20 14:56:30 -0300

    configuracao browser-sync e nodemon

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const server = express();
 
 const data = require('./data');
 const images = {
-    logo: '/images/logo.png',
-    chef: '/images/chef.png'
-}
+  logo: '/images/logo.png',
+  chef: '/images/chef.png',
+};
 
 server.use(express.static('public'));
 server.use('/images', express.static('./layouts/assets'));
@@ -15,30 +15,30 @@ server.use('/images', express.static('./layouts/assets'));
 server.set('view engine', 'njk');
 
 nunjucks.configure('views', {
-    express: server,
-    noCache: true,
-    autoescape: false
-})
+  express: server,
+  noCache: true,
+  autoescape: false,
+});
 
-server.get('/', function(req, res){
-    return res.render('home', {recipes: data, images});
-})
+server.get('/', function (req, res) {
+  return res.render('home', { recipes: data, images });
+});
 
-server.get('/about', function(req, res){
-    return res.render('about', {images});
-})
+server.get('/about', function (req, res) {
+  return res.render('about', { images });
+});
 
-server.get('/recipes', function(req, res){
-    return res.render('recipes', {recipes: data, images});
-})
+server.get('/recipes', function (req, res) {
+  return res.render('recipes', { recipes: data, images });
+});
 
-server.get('/recipes/:index', function(req, res){
-    const recipes = data;
-    const recipeIndex = req.params.index;
-    const recipe = recipes[recipeIndex];
-    return res.render('recipe_detail', {recipe, images});
-})
+server.get('/recipes/:index', function (req, res) {
+  const recipes = data;
+  const recipeIndex = req.params.index;
+  const recipe = recipes[recipeIndex];
+  return res.render('recipe_detail', { recipe, images });
+});
 
-server.listen(5002, function(){
-    console.log('server is running');
-})
+server.listen(5000, function () {
+  console.log('server is running');
+});
